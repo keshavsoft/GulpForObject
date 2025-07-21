a1cc8bbc20e2e7f733b374d7b8606d4d908d45f4
Srivasu215 <sri.vasu215@gmail.com>
2025-07-21 15:12:10 +0530

    Some Ui Changes

--- src/js/CrudOnObject/FCreate/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/TableFooterSaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/js/CrudOnObject/FCreate/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/TableFooterSaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -1,4 +1,11 @@
 let StartFunc = () => {
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your record has been saved",
+        showConfirmButton: false,
+        timer: 1500
+    });
     let jVarLocalRefreshBSTableId = document.querySelector(`.RefreshBSTableClass`);
     jVarLocalRefreshBSTableId.click();
 };
